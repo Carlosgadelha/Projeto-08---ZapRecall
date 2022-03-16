dc4e828ab5ae8f88d8b5ea36e00df9deb5e3968d
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-03-16 08:24:14 -0300

    arquivos inciais

---
+++ src/Components/App.jsx
@@ -0,0 +1,10 @@
+export default function App(){
+    return (
+        <div className="inicio">
+            
+            <img src="assets/img/logo.png"/>
+            <h1>ZapRecall</h1>
+            <button name="button">Iniciar Recall!</button>
+        </div>
+    )
+}
